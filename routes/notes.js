ccb7497c44995461008c20960d8d195a6838801b
Owen Lang <owen.lang146@gmail.com>
2023-08-30 14:16:51 -0500

    added notes route

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { username, topic, tip } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      username,
+      tip,
+      topic,
+      tip_id: uuid(),
+    };
+
+    readAndAppend(newTip, './db/db.json');
+    res.json(`Tip added successfully`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+
+module.exports = notes;
